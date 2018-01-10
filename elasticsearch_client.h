11157db56d09120fb34a286c371e5bb1a3e0bf94
ruan.sheng <ruan.sheng@immomo.com>
2018-01-10 16:43:51 +0800

    fix

--- elasticsearch_client.h
+++ elasticsearch_client.h
@@ -10,6 +10,6 @@ PHPAPI void es_client_add_parse(INTERNAL_FUNCTION_PARAMETERS, zend_string **requ
 PHPAPI void es_client_add_request(INTERNAL_FUNCTION_PARAMETERS, zend_string **request_url, zval **zv_body);
 
 static inline void trace(const char *file, int line, const char* function, const char *fmt, ...);
-#define TRACE(fmt, …) do{trace(__FILE__, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__); }while(0)
+#define TRACE(fmt, ...) do{trace(__FILE__, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__); }while(0)
 
 #endif //ELASTICSEARCH_UTIL_H
